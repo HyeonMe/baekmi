42e77bd0507bd76e9cde906b6b00ee4b739cb912
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-08-29 22:56:37 +0900

    틀 제작중..

--- white-eyebrows/src/App.js
+++ white-eyebrows/src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1>test</h1>
+    <React.Fragment>
+      <h1 class="logo">로고</h1>
+      <button class="Category">만화</button>
+      <button class="Category" style={{top: 200+'px'}}>소설</button>
+      <p class="Category" style={{top: 294+'px',background: 'none',align: 'center'}}>검색</p>
+      <button class="Category" style={{top: 753+'px'}}>로그인/회원가입</button>
+      <button class="Category" style={{top: 832+'px'}}>내정보</button>
+
+      <div class="main-Div">
+        
+      </div>
+
+      <div class="side-Web-name">
+            <span style={{color: 'rgb(45, 218, 195)'}}>로고</span> Web Site
+        </div>
+    </React.Fragment>
   );
 }
 
